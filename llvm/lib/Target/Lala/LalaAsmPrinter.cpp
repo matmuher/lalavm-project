847f8817f926b0458be3231961f6e3cd50cf481f
Matveich <matmuh316@gmail.com>
2025-03-23 19:52:44 +0300

    Add AsmPrinter

---
+++ llvm/lib/Target/Lala/LalaAsmPrinter.cpp
@@ -0,0 +1,61 @@
+#include "MCTargetDesc/LalaInstPrinter.h"
+#include "Lala.h"
+#include "LalaSubtarget.h"
+#include "LalaTargetMachine.h"
+#include "TargetInfo/LalaTargetInfo.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/CodeGen/AsmPrinter.h"
+#include "llvm/CodeGen/MachineConstantPool.h"
+#include "llvm/CodeGen/MachineFunctionPass.h"
+#include "llvm/CodeGen/MachineInstr.h"
+#include "llvm/CodeGen/MachineModuleInfo.h"
+#include "llvm/MC/MCAsmInfo.h"
+#include "llvm/MC/MCInst.h"
+#include "llvm/MC/MCStreamer.h"
+#include "llvm/MC/MCSymbol.h"
+#include "llvm/MC/TargetRegistry.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "asm-printer"
+
+namespace {
+
+class LalaAsmPrinter : public AsmPrinter {
+  const MCSubtargetInfo *STI;
+
+public:
+  explicit LalaAsmPrinter(TargetMachine &TM,
+                         std::unique_ptr<MCStreamer> Streamer)
+      : AsmPrinter(TM, std::move(Streamer)), STI(TM.getMCSubtargetInfo()) {
+    Lala_DUMP_GREEN
+  }
+
+  void emitInstruction(const MachineInstr *MI) override;
+
+  StringRef getPassName() const override { return "Lala Assembly Printer"; }
+
+  bool lowerPseudoInstExpansion(const MachineInstr *MI, MCInst &Inst);
+};
+
+} // end anonymous namespace
+
+// Simple pseudo-instructions have their lowering (with expansion to real
+// instructions) auto-generated.
+#include "LalaGenMCPseudoLowering.inc"
+
+void LalaAsmPrinter::emitInstruction(const MachineInstr *MI) {
+  Lala_DUMP_GREEN
+  // Do any auto-generated pseudo lowerings.
+  if (MCInst OutInst; lowerPseudoInstExpansion(MI, OutInst)) {
+    EmitToStreamer(*OutStreamer, OutInst);
+    return;
+  }
+}
+
+// Force static initialization.
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeLalaAsmPrinter() {
+  RegisterAsmPrinter<LalaAsmPrinter> X(getTheLalaTarget());
+}
